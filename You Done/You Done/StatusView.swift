2497f870c6165eeeb76bfda115709e63768282ed
Piotr Galar <pgalar@sumologic.com>
2020-12-09 14:39:57 +0100

    Added date picker

--- You Done/You Done/StatusView.swift
+++ You Done/You Done/StatusView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct StatusView: View {
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        return formatter
+    }
+
+    @State private var date = Date()
+    @State private var showDatePicker = false
+    @State private var prevDate: Date?
+    
+    func dateString() -> String { dateFormatter.string(from: date) }
+
     var body: some View {
-        Text("Status")
+        VStack {
+            HStack {
+                Text(dateString())
+                    .onTapGesture {
+                        showDatePicker.toggle()
+                    }
+                    .popover(
+                        isPresented: $showDatePicker,
+                        arrowEdge: .bottom
+                    ) {
+                        DatePicker(dateString(), selection: $date, in: ...Date(), displayedComponents: .date).datePickerStyle(GraphicalDatePickerStyle())
+                            .labelsHidden()
+                    }
+                    
+                Spacer()
+            }
+            Spacer()
+        }
     }
 }
 
